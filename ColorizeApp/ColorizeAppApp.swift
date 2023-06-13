7ebbdc446c44d2372a246c28a25dad709c742925
aleksei <alikexie93@yandex.ru>
2023-06-13 20:47:01 +0600

    Initial Commit

---
+++ ColorizeApp/ColorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizeAppApp.swift
+//  ColorizeApp
+//
+//  Created by Алексей Филиппов on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
